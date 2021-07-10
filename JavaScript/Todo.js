c65cf54b496ec30b52897015c9c8e84d06d7719a
Sungmin Oh <80186242+AwesomeMin@users.noreply.github.com>
2021-07-10 10:10:56 +0900

    Add files via upload

--- JavaScript/Todo.js
+++ JavaScript/Todo.js
@@ -4,17 +4,54 @@ const contentInput = toDoForm.querySelector("#ContentInput")
 const toDoList = document.querySelector("ul");
 let toDos = [];
 
+function handleChangeDegree(event) {
+    console.log(event.target.parentElement.children);
+    event.target.parentElement.children[3].classList.remove("hidden");
+    event.target.parentElement.children[4].classList.add("hidden");
+    toDos.forEach(toDo => {
+        if (toDo.id === parseInt(event.target.parentElement.id)) {
+            toDo.degree = 0;
+        }
+    })
+    saveToDos();
+}
+
+function handleDegree(event) {
+    const degreeText = event.target.parentElement.children[4];
+    if (event.target.value > 100 || event.target.value < 0) {
+        event.target.value = null;
+        alert("1과 100 사이의 숫자를 입력해주세요.");
+    }
+    toDos.forEach(toDo => {
+        if (toDo.id === parseInt(event.target.parentElement.id)) {
+            toDo.degree = parseInt(event.target.value);
+        }
+    })
+    saveToDos();
+    event.target.classList.add("hidden");
+    degreeText.classList.remove("hidden");
+    degreeText.innerText = `${event.target.value}%`
+    event.path[1].insertBefore(degreeText, event.path[1].children[5]);
+}
+
 function handleCbox(event) {
     const li = event.target.parentElement;
     const title = event.path[1].children[0];
-    const content = event.path[1].children[3];
+    const content = event.path[1].children[6];
+    const degree = event.path[1].children[3];
+    const degreeText = event.path[1].children[4];
+    degree.classList.toggle("hidden");
     title.classList.toggle("done");
     content.classList.toggle("done");
     toDos.forEach(toDo => {
         if(toDo.id === parseInt(li.id) && event.target.checked) {
             toDo.done = 1;
+            degree.classList.remove("hidden");
         } else if (toDo.id === parseInt(li.id) && !event.target.checked){
             toDo.done = 0;
+            toDo.degree = 0;
+            degreeText.classList.add("hidden");
+            degree.classList.add("hidden");
         }
     })
     saveToDos();
@@ -32,26 +69,42 @@ function deleteToDo(event) {
 }
 
 function addNewToDo(obj) {
+    const br = document.createElement("br");
     const li = document.createElement("li");
     li.id = obj.id;
     const title = document.createElement("span");
     title.innerText = obj.title;
     const content = document.createElement("span");
-    content.innerHTML = `<br>${obj.content}`;
+    content.innerText = obj.content;
     const button = document.createElement("button");
     button.addEventListener("click", deleteToDo);
-    button.innerText = "delete"
+    button.innerText = "delete";
     const cbox = document.createElement("input");
-    cbox.type="checkbox"
+    cbox.type="checkbox";
     cbox.addEventListener("click", handleCbox);
+    const degree = document.createElement("input");
+    degree.type="number";
+    degree.classList.add("hidden");
+    degree.placeholder="몇 퍼센트 완료했나요?";
+    degree.addEventListener("change", handleDegree);
+    const degreeText = document.createElement("span");
+    degreeText.classList.add("hidden");
+    degreeText.addEventListener("click", handleChangeDegree);
     if(obj.done === 1) {
         title.classList.add("done");
         content.classList.add("done");
+        degree.classList.remove("hidden");
         cbox.checked = true;
+        degree.classList.add("hidden");
+        degreeText.innerText = `${obj.degree}%`;
+        degreeText.classList.remove("hidden");
     }
     li.appendChild(title);
     li.appendChild(cbox);
     li.appendChild(button);
+    li.appendChild(degree);
+    li.appendChild(degreeText);
+    li.appendChild(br);
     li.appendChild(content);
     toDoList.appendChild(li);
 }
@@ -65,7 +118,8 @@ function handleInputForm(event) {
         content: content,
         id: Date.now(),
         done: 0,
-        date: DateDisplay.innerText
+        date: DateDisplay.innerText,
+        degree: 0
     }
     addNewToDo(newToDoObject);
     titleInput.value = "";
